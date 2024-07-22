7630ef83fc10dddd349943fbf5059f79ca9f885c
AstroGamers-0217-2020 <hk643@nau.edu>
2024-07-22 12:53:03 -0700

    modified

--- jsonusage.js
+++ jsonusage.js
@@ -1,37 +1,60 @@
+const http = require('http');
+
 const movies_json = `{
     "movies": [
-       {
-         "title": "Running Scared",
-         "director": "Wayne Kramer",
-         "year": 2006,
-         "genre": "Action",
-         "actors": ["Paul Walker", "Cameron Bright", "Chazz Palminteri"]
-       },
-       {
-         "title": "500 days of summer",
-         "director": "Marc Webb",
-         "year": 2009,
-         "genre": "Comedy",
-         "actors": ["Zooey Deschanel", "Joseph Gordon-Levitt", "Geoffrey Arend"]
-       },
-       {
-         "title": "Kung Fu Hustle",
-         "director": "Stephen Chow",
-         "year": 2004,
-         "genre": "Fantasy",
-         "actors": ["Stephen Chow", "Wah Yuen", "Qiu Yuen"]
-       }
-     ]
-   }`;
+      {
+        "title": "Running Scared",
+        "director": "Wayne Kramer",
+        "year": 2006,
+        "genre": "Action",
+        "actors": ["Paul Walker", "Cameron Bright", "Chazz Palminteri"]
+      },
+      {
+        "title": "500 days of summer",
+        "director": "Marc Webb",
+        "year": 2009,
+        "genre": "Comedy",
+        "actors": ["Zooey Deschanel", "Joseph Gordon-Levitt", "Geoffrey Arend"]
+      },
+      {
+        "title": "Kung Fu Hustle",
+        "director": "Stephen Chow",
+        "year": 2004,
+        "genre": "Fantasy",
+        "actors": ["Stephen Chow", "Wah Yuen", "Qiu Yuen"]
+      }
+    ]
+  }`;
 
 const movies = JSON.parse(movies_json);
-const movies_string = JSON.stringify(movies, null, 2);
 
-console.log(movies.movies[1].title);
-console.log(movies.movies[1].director);
-console.log(movies.movies[1].actors);
-console.log(movies.movies[2].title);
-console.log(movies.movies[2].director);
-console.log(movies.movies[2].year);
+// Creating the HTTP server
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Movies Data</h1>');
+        res.write('<h2>JSON Data:</h2>');
+        res.write('<pre>' + JSON.stringify(movies, null, 2) + '</pre>');
 
-console.log(movies_string);
+        // Adding console.log outputs to the response
+        res.write('<h2>Console Log Outputs:</h2>');
+        res.write('<pre>');
+        res.write(`${movies.movies[1].title}\n`);
+        res.write(`${movies.movies[1].director}\n`);
+        res.write(`${movies.movies[1].actors}\n`);
+        res.write(`${movies.movies[2].title}\n`);
+        res.write(`${movies.movies[2].director}\n`);
+        res.write(`${movies.movies[2].year}\n`);
+        res.write('</pre>');
+
+        res.end();
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page Not Found');
+    }
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
